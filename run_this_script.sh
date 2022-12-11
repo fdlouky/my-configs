dc244ccf60ac5bcdb32db5a5ec6d19cfb4033a4b
fdlouky <fdlouky@bmat.com>
2022-12-11 16:51:00 +0100

    Fix commands to install wifi driver

--- run_this_script.sh
+++ run_this_script.sh
@@ -35,13 +35,15 @@ echo "Solve Wifi drivers? (Lenovo issue)"
 select yn in "yes" "no"; do
 	case $yn in
 		yes ) sudo apt install -y git linux-headers-generic build-essential dkms
-			git clone https://github.com/Mange/rtl8192eu-linux-driver $HOME
-			sudo dkms add $HOME/rtl8192eu-linux-driver
-			sudo dkms install $HOME/rtl8192eu-linux-driver/rtl8192eu/1.0
+			git clone https://github.com/Mange/rtl8192eu-linux-driver
+			cd rtl8192eu-linux-driver
+			sudo dkms add .
+			sudo dkms install rtl8192eu/1.0
 			echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf
 			echo -e "8192eu\n\nloop" | sudo tee /etc/modules
 			echo "options 8192eu rtw_power_mgnt=0 rtw_enusbss=0" | sudo tee /etc/modprobe.d/8192eu.conf
 			sudo update-grub; sudo update-initramfs -u
+			cd $dir
 			break;;
 		no ) break;;
 	esac
