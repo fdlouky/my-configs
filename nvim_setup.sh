114816fd35e20b62f5314b3c45f82d0470a9dcdc
fdlouky <fmdlouky@gmail.com>
2022-12-17 19:11:48 +0100

    Remove commands moved to run_this_script.sh and install latest release (nightly)

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -16,41 +16,15 @@ ln -sf $dir/coc-settings.json $nvim_configs/coc-settings.json
 # nvim_after_directory=$nvim_configs/after/plugin
 # ln -sf $dir/tabout.vim $nvim_after_directory/tabout.vim
 
-# Install nerd fonts
-mkdir -p $HOME/.local/share/fonts
-sudo apt install fonts-powerline
-cd $HOME/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+# Install NeoVim latest prerelease
+curl -LO  https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb
+sudo apt install ./nvim-linux64.deb
+rm nvim-linux64.deb
 
-# Install NeoVIm stuffs
-sudo apt install -y exuberant-ctags nodejs
-echo "is npm included in nodejs installation?"
-select yn in "yes" "no"; do
-	case $yn in
-		no ) sudo apt install -fy npm
-			sudo npm install -g npm@latest
-			sudo npm cache clean -f
-			sudo npm install -g n
-			sudo n stable
-			break;;
-		yes ) break;;
-	esac
-done
-sudo npm install -g npm
-sudo npm install -g yarn
-echo | sudo add-apt-repository ppa:neovim-ppa/stable -y
-sudo apt-get update
-sudo apt install -y neovim bat
+# Install Plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim +'PlugInstall --sync' +qa
 nvim +'UpdateRemotePlugins' +qa
 nvim +'CocInstall -sync coc-pyright coc-prettier coc-git coc-json coc-docker coc-yaml coc-highlight coc-sql' +qa
 nvim +CocUpdateSync +qa
-
-# To remove all related nodejs and npm
-# sudo apt --purge remove nodejs npm
-# sudo apt clean
-# sudo apt autoclean
-# sudo apt -f install
-# sudo apt autoremove
-# sudo rm -rf /usr/lib/node_modules/npm/
